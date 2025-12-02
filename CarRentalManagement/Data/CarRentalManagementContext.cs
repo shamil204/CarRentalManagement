eb25567ff3902a65f8c1990afb215da8fde2f6ba
shamil <2402299H@student.tp.edu.sg>
2025-12-02 14:56:02 +0800

    added custom identity logic

--- CarRentalManagement/Data/CarRentalManagementContext.cs
+++ CarRentalManagement/Data/CarRentalManagementContext.cs
@@ -1,4 +1,5 @@
 ﻿using CarRentalManagement.Configuration.Entities;
+using CarRentalManagement.Configurations.Entities;
 using CarRentalManagement.Data;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
@@ -20,6 +21,9 @@ namespace CarRentalManagement.Data
             builder.ApplyConfiguration(new ColourSeed());
             builder.ApplyConfiguration(new MakeSeed());
             builder.ApplyConfiguration(new ModelSeed());
+            builder.ApplyConfiguration(new UserSeed());
+            builder.ApplyConfiguration(new UserRoleSeed());
+            builder.ApplyConfiguration(new RoleSeed());
         }
     }
 }
